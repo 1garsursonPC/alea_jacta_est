464b865529d103fcd704c17b224979991f03a5e1
Gehu <lauric.gehu@free.fr>
2023-12-12 10:23:00 +0100

    Création de BasicEncrypt et modification des surcharges de encrypt

---
+++ encrypt.cpp
@@ -0,0 +1,57 @@
+#include "encrypt.h"
+
+#include <istream>
+#include <ostream>
+#include <fstream>
+#include <string>
+
+namespace aje
+{
+    Encrypt::Encrypt(const std::string& plain) : _plain(plain) 
+    {
+    }
+
+    const std::string& Encrypt::operator<<(const std::string& in)
+    {
+        _plain = in;
+        encode();
+        return _cipher;
+    }
+
+    std::string& Encrypt::operator>>(std::string& out)
+    {
+        out += _cipher;
+        return out;
+    }
+
+    bool read(const std::string& filename, std::string& buffer)
+    {
+        std::ifstream in(filename, std::ifstream::in);
+        in >> buffer;
+        in.close();
+        return true;
+    }
+
+    bool write(const std::string& filename, const std::string& data)
+    {
+        std::ofstream out(filename, std::ofstream::out);
+        out << data;
+        out.close();
+        return true;
+    }
+
+    BasicEncrypt::BasicEncrypt(const std::string& plain) : Encrypt(plain)
+    {
+        encode();
+    } 
+
+    void BasicEncrypt::encode()
+    {
+        _cipher = _plain;
+    }
+
+    void BasicEncrypt::decode()
+    {
+        _plain = _cipher;
+    }
+} // namespace aje
--- encrypt.h
+++ encrypt.h
@@ -1,12 +1,14 @@
 #ifndef ENCRYPT_H_
 #define ENCRYPT_H_
 
+#include <string>
+
 namespace aje
 {
     class Encrypt
     {
         public:
-            Encrypt(const std::string& plain) : _plain(plain) {}
+            Encrypt(const std::string& plain);
 
             const std::string& cipher() const { return _cipher; }
             const std::string& plain() const { return _plain; }
@@ -14,14 +16,23 @@ namespace aje
             virtual void encode() = 0;
             virtual void decode() = 0;
             
+            std::string& operator>>(std::string& out);
+            const std::string& operator<<(const std::string& in);
+        
         protected:
 
             std::string _cipher;
             std::string _plain; 
     };
 
-    std::ostream& operator<<(std::ostream& out, Encrypt& en);
-    std::istream& operator>>(std::istream& in, Encrypt& en);
+    class BasicEncrypt : public Encrypt
+    {
+        public:
+            BasicEncrypt(const std::string& plain);
+
+            void encode() override;
+            void decode() override;
+    };
 
     bool read(const std::string& filename, std::string& buffer);
     bool write(const std::string& filename, const std::string& data);
